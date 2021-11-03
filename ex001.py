56995f2286945d531407573619edde3757dd633c
crizostomo <diogo_crizostomo@hotmail.com>
2021-11-03 20:32:53 -0300

    new codes have been added

--- ex001.py
+++ ex001.py
@@ -1,7 +1,7 @@
 name = input('Hey, what is your name?')
 print('It is nice to meet you', name)
 age = input('How old are you')
-print('Oh, so you are', age, '!', 'I am 30 years old', name)
+print('Oh, so you are', age, '!', 'I am Python and I am 30 years old', name)
 adress = input('What is your address?')
 print("The address informed was:", adress)
 print('\n-----------------')
---
+++ ex003.py
@@ -0,0 +1,3 @@
+value1 = input('please, type a number')
+value2 = input('please, type another one')
+print('the sum of them are', value1+value2)
